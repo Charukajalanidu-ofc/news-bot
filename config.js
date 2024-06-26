6271f7a5cd423a44dd9ccb8259307cff1c993093
lenoxofc1 <ofclenox@gmail.com>
2024-06-26 12:43:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GRO
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
 ADDRESSES: process.env.ADDRESSES === undefined ? '' : process.env.ADDRESSES,
 ALIVE: process.env.ALIVE === undefined ? `Hello im alive now !!` : process.env.ALIVE,
-OWNER: process.env.OWNER === undefined ? `9471567890` : process.env.OWNER,
+OWNER: process.env.OWNER === undefined ? `94787318729` : process.env.OWNER,
 PREFIX: process.env.PREFIX === undefined ? '@' : process.env.PREFIX,
 FOOTER: process.env.FOOTER=== undefined ? '‌': process.env.FOOTER,
 LOGO: process.env.LOGO === undefined ? `https://telegra.ph/file/f25fa1b13e9962f2c2271.jpg` : process.env.LOGO
